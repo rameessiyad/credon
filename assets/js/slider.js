d47a6d7054166d8b1e16a0b91111612a6fa71f9c
rameessiyad <rameesmsiyadofficial786@gmail.com>
2025-08-21 09:59:52 +0530

    career banner img

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,27 +1,3 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   const sliderWrapper = document.querySelector(".slider-wrapper");
-//   const slides = document.querySelectorAll(".slider-wrapper .slide");
-//   const prevBtn = document.querySelector(".custom-prev");
-//   const nextBtn = document.querySelector(".custom-next");
-
-//   let currentIndex = 0;
-//   const totalSlides = slides.length;
-
-//   function showSlide(index) {
-//     if (index < 0) index = totalSlides - 1;
-//     if (index >= totalSlides) index = 0;
-//     currentIndex = index;
-
-//     const offset = -index * 100;
-//     sliderWrapper.style.transform = `translateX(${offset}%)`;
-//   }
-
-//   prevBtn.addEventListener("click", () => showSlide(currentIndex - 1));
-//   nextBtn.addEventListener("click", () => showSlide(currentIndex + 1));
-
-//   showSlide(0);
-// });
-
 $(".slider-wrapper").slick({
   infinite: true,
   slidesToShow: 1,
